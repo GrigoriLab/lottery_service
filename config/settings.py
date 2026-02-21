b49a1909ebf6f47459807b052a8dc8516970f951
Grigori Kartashyan <grigori.kartashyan@gmail.com>
2026-02-21 21:15:37 +0100

    Move django apps into apps package

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "lottery"
+    "apps.lottery",
+    "apps.api",
 ]
 
 MIDDLEWARE = [
