9e89190dc6e30afb3883ca4442566dac2e3040d8
Grigori Kartashyan <grigori.kartashyan@gmail.com>
2026-02-22 16:44:39 +0100

    Add make command to setup periodic tasks

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: tests lint format migrate collectstatic createsuperuser db_and_redis load_test_data
+.PHONY: tests lint format migrate collectstatic createsuperuser db_and_redis load_test_data setup_periodic_tasks
 
 tests:
 	poetry install --no-root --no-interaction
@@ -21,6 +21,9 @@ createsuperuser:
 
 load_test_data:
 	docker compose --env-file .env.docker exec lottery_service python manage.py create_test_data
+
+setup_periodic_tasks:
+	docker compose --env-file .env.docker exec lottery_service python manage.py setup_periodic_tasks
 
 db_and_redis:
 	docker compose up -d db redis
--- apps/lottery/management/commands/setup_periodic_tasks.py
+++ apps/lottery/management/commands/setup_periodic_tasks.py
@@ -1,18 +1,9 @@
 from django.core.management.base import BaseCommand
-from django_celery_beat.models import CrontabSchedule, IntervalSchedule, PeriodicTask
+from django_celery_beat.models import CrontabSchedule, PeriodicTask
 
 
 class Command(BaseCommand):
     help = "Set up periodic tasks"
-
-    def add_arguments(self, parser):
-        parser.add_argument(
-            "--test",
-            action="store_true",
-            dest="is_test",
-            default=False,
-            help="Create lottery and set up periodic task for 10 seconds",
-        )
 
     def handle(self, *args, **options):
         crontab_schedule, _ = CrontabSchedule.objects.get_or_create(
@@ -20,20 +11,10 @@ class Command(BaseCommand):
         )
 
         task, task_created = PeriodicTask.objects.get_or_create(
-            name="Close lottery and pick winner",
+            name="Close lottery",
             defaults={
                 "crontab": crontab_schedule,
                 "task": "apps.lottery.tasks.close_lottery",
             },
         )
-        if options["is_test"]:
-            test_schedule, _ = IntervalSchedule.objects.get_or_create(
-                every=30,
-                period=IntervalSchedule.SECONDS,
-            )
-            task.interval = test_schedule
-            task.crontab = None
-        else:
-            task.interval = None
-            task.crontab = crontab_schedule
         task.save()
